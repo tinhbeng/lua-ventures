cd31d214b23bc25d8cfb9c4baa8374d1047350b6
tinhbeng <tinhns@vietbrain.com>
2021-08-04 23:47:22 +0700

    js.js

---
+++ assets/js/js.js
@@ -0,0 +1,103 @@
+'use strict'
+
+jQuery(document).ready(function ($) {
+    $('.partner-box').slick({
+        dots: true,
+        rows: 2,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $('.portfolio-box').slick({
+        dots: true,
+        rows: 2,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $('.nav-icon').on('click', function () {
+        let element = $(this).closest('.nav-wrap').find('.navigation');
+        if (element.hasClass('show')) {
+            element.removeClass("show");
+        } else {
+            element.addClass("show");
+        }
+
+    })
+    $('.is-child').on('click', function (e) {
+        e.preventDefault();
+        $(this).find('> ul').toggleClass('dropdown');
+    })
+    $(document).mouseup(function (e) {
+        var container = $(".navigation.show");
+        var icon = $('.icon-mobile');
+
+        // if the target of the click isn't the container nor a descendant of the container
+        if (icon.is(e.target) === false && !container.is(e.target) && container.has(e.target).length === 0) {
+            if (container.hasClass('show')) {
+                container.removeClass("show");
+            } else {
+                container.addClass("show");
+            }
+        }
+    });
+
+    $(".nav-item > a").on('click', function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        let element = $(this).closest('.nav-wrap').find('.navigation');
+        if (element.hasClass('show')) {
+            element.removeClass("show");
+        }
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
